a66bbe60b9983690a918ad12cf57682eff61c4df
Marty-jr-95 <martymuhangajr@outlook.com>
2022-01-09 22:28:22 +0200

    phase complete

---
+++ components/Footer.jsx
@@ -0,0 +1,20 @@
+import {Box} from '@chakra-ui/react'
+
+const Footer = () => {
+    return (
+      <Box
+        textAlign="center"
+        p="5"
+        color="gray.800"
+        borderTop="1px"
+        borderColor="gray.100"
+        shadow="sm"
+        mt='6'
+      >
+         © 2022 IntelliRealtor Inc.
+        
+      </Box>
+    );
+}
+
+export default Footer
---
+++ components/Navbar.jsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Flex,
+  Box,
+  Spacer,
+} from '@chakra-ui/react';
+import { FcMenu, FcHome, FcAbout } from 'react-icons/fc';
+import { BsSearch } from 'react-icons/bs';
+import { FiKey } from 'react-icons/fi';
+
+const Navbar = () => {
+  return (
+    <Flex p="3" borderBottom="1px" borderColor="gray.100" shadow="sm">
+      <Box fontSize="3xl" color="teal.300" fontWeight="bold">
+        <Link href="/" paddingLeft="3">
+          IntelliRealtor
+        </Link>
+      </Box>
+      <Spacer />
+      <Box>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<FcMenu />}
+            variant="outline"
+            color="red.400"
+          />
+          <MenuList>
+            <Link href="/" passHref>
+              <MenuItem icon={<FcHome />}>Home</MenuItem>
+            </Link>
+            <Link href="/search" passHref>
+              <MenuItem icon={<BsSearch />}>Search</MenuItem>
+            </Link>
+            <Link href="/search?purpose=for-sale" passHref>
+              <MenuItem icon={<FcAbout />}>Buy Property</MenuItem>
+            </Link>
+            <Link href="/search?purpose=for-rent" passHref>
+              <MenuItem icon={<FiKey />}>Rent Property</MenuItem>
+            </Link>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
